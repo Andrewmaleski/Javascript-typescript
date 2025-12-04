c5375e92301002ee8ff734562d87d5c90b2b5187
Andrew Maleski <andrewmaleski@gmail.com>
2025-12-04 20:04:34 -0300

    Validando campos senhas e enviando formulario

--- aula52/assets/js/main.js
+++ aula52/assets/js/main.js
@@ -14,7 +14,33 @@ class ValidaFormulario{
     handleSubmit(e){
         e.preventDefault();
         const camposValidos = this.camposSaoValidos();
-     
+        const senhasValidas = this.senhasSaoValidas();
+        
+        if (camposValidos && senhasValidas){
+            alert('Formulario Enviado')
+            this.formulario.submit();
+        }
+    }
+
+    senhasSaoValidas(){
+        let valid = true;
+
+        const senha1 = this.formulario.querySelector('.senha');
+        const senha2 = this.formulario.querySelector('.repetir-senha');
+
+        if (senha1.value !== senha2.value){
+            this.criaErro(senha1, "Senhas devem ser iguais");
+            this.criaErro(senha2, "Senhas devem ser iguais");
+            valid = false;
+        }
+
+        if (senha1.value.length < 6 || senha2.value.length > 12){
+            this.criaErro(senha1, "Senha precisa ter entre 6 e 12 caracteres");
+            this.criaErro(senha2, "Senha precisa ter entre 6 e 12 caracteres");
+            valid = false;
+        }
+
+        return valid;
     }
 
     camposSaoValidos(){
@@ -38,16 +64,25 @@ class ValidaFormulario{
                 if(!this.validaUsuario(campo)) valid = false;
             }
         }
+
+        return valid;
     }
 
     validaUsuario(campo){
-        const user = campo.value.lenght;
+        const user = campo.value;
+        let valid = true;
 
-        if (user >= 3 && user <= 12) {
-            return true;
-        } else {
-            this.criaErro(campo, 'Usuario devera conter entre 3 e 12 caracteres')
-        }
+        if (user.length < 3 || user.length > 12) {
+            this.criaErro(campo, 'Usuario devera conter entre 3 e 12 caracteres');
+            valid = false;
+        } 
+
+        if (!user.match(/^[a-zA-Z0-9]+$/g)) {
+            this.criaErro(campo, 'Nome de usuario precisa conter apenas letras e numeros');
+            valid = false;
+        } 
+
+        return valid;
 
     }
 
