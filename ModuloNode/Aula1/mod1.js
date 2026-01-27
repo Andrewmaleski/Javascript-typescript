635bb9af3cb84e7b3c7825b6c5a7ef0db3d22c9c
Andrew Maleski <andrewmaleski@gmail.com>
2026-01-27 17:10:06 -0300

    Verificando documentos sobre node e npm.

---
+++ ModuloNode/Aula1/mod1.js
@@ -0,0 +1,29 @@
+//utilizar module.exports.nome = nome(nome que vem do arquivo) para exportar funções e classes para outros arquivos. 
+//no arquivo que você quer puxar a classe ou função colocar const ex1 = require('./ex1');
+
+
+// function soma(a, b) {
+//   return a + b;
+// }
+
+// module.exports.soma = soma;
+
+// ou exportar varias ContentVisibilityAutoStateChangeEvent
+// function soma() {}
+// function sub() {}
+
+// module.exports = {
+//   soma,
+//   sub
+// };
+
+
+// importando 
+// const ex1 = require('./ex1');
+
+// ex1.soma(2, 3);
+
+// importando direto 
+// const Pessoa = require('./Pessoa');
+
+// const p = new Pessoa('Andreu');
